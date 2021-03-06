0833d4d6b341d572926cb149adcb5fb3da15504f
pranavchandran <pranav.chandran@gmail.com>
2021-03-06 09:10:32 +0530

    Email Completed and started with comments section

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -36,17 +36,17 @@ def post_share(request, post_id):
     post = get_object_or_404(Post, id=post_id, status='published')
     sent = False
     if request.method == 'POST':
-        # form was submitted
+        # Form was submitted
         form = EmailPostForm(request.POST)
         if form.is_valid():
             # Form fields passed validation
             cd = form.cleaned_data
-            post_url = post.get_absolute_url()
-            subject = f"{cd[name]} recommends you read"\
-                        f"{post.title}"
-            message = f"Read {post.title} at {post_url}\n\n"\
-                        f"{cd['name']}\'s comments:{cd['comments']}"
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{cd['name']} recommends you read {post.title}"
+            message = f"Read {post.title} at {post_url}\n\n" \
+                      f"{cd['name']}\'s comments: {cd['comments']}"
             send_mail(subject, message, 'admin@myblog.com', [cd['to']])
+            sent = True
     else:
         form = EmailPostForm()
     return render(request, 'blog/post/share.html', {'post':post, 'form':form, 'sent':sent})
